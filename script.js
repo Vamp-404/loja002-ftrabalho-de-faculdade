b5b54f694ad3d99695a3af6ba440279dd2f1fbc4
Vamp-404 <gustavowendellrochal@gmail.com>
2025-11-12 19:35:46 -0300

    mouseouver

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ const imgAberta = 'icewizard.png';
 
 imagem.addEventListener('mouseover', function() {
     // Muda a imagem para a boca aberta
-    imagem.src = imagens/icewizard.png; 
+    imagem.src = 'imagens/icewizard.png'; 
 });
 
 imagem.addEventListener('mouseout', function() {
     // Volta a imagem para a boca fechada
-    imagem.src = imagens/caveirabocafechadagu.png;
+    imagem.src = 'imagens/caveirabocafechadagu.png';
 });
